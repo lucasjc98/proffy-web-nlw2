0fcb6cfeba5f79f432bf705c2c4001c915aaa0e9
Lucas Costa <lucasjcosta.98@gmail.com>
2020-08-05 20:31:18 -0300

    NLW#2

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,53 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+import './styles.css';
+
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher; 
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    function createNewConncetion() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/36082328?s=460&u=6cf087fae03195e82e2d352d70d754f5cdb5e51a&v=4" alt="Lucas Costa" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Lucas Costa</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet.
-                <br /><br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in tempus nisi. Nunc congue finibus lacus nec bibendum. Cras convallis pharetra erat ut volutpat.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 70,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConncetion} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
